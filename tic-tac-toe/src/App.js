66a1359f4a23e6e85d4e1b04bceff5e5925de464
royboot <rgrizell467@gmail.com>
2023-12-13 17:02:38 +0100

    almost finished bug fix
    made even more changes to bug fixes

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,58 +1,76 @@
 import './App.css';
 import{useState}from  'react';
-const[squares,setSquares] =  useState(Array(9).fill(null));
 
-function Restart(squares){
 
-  function restart(){
-    for(let i = 0; i<squares.length; i++){
-      squares[i] = null;
-    }
-  }
-  return <button className='restart' onClick={restart}>Restart</button>
-}
 //makes a new function react component
 function Square({value,onSquareClick}){
   return <button className = "Square" onClick={onSquareClick}>{value}</button>
 }
 //default keyword makes this function the main function to execute
 export default function Board() {
+  const[squares,setSquares] =  useState(Array(9).fill(null));
   const[xIsNext,setXNext] = useState(true);
-  const Winner = calcWinner(squares);
-  let status;
-  if(Winner){
-    status= `Game over Winner is:${Winner}`;
-  }else{
-    // if xIsNext set status to x else to O
-    status =  "Next player: "+(xIsNext?"X":"O")
+
+  function calcWinner(squares){
+    let GameOver = null;
+    const lines = [
+      [0, 1, 2],[3, 4, 5],[6, 7, 8],
+      [0, 3, 6],[1, 4, 7],[2, 5, 8],
+      [0, 4, 8],[2, 4, 6]
+    ];
+    for(let i = 0; i<lines.length; i++){
+      const[a, b, c]= lines[i];
+      if(squares[a] && squares[a]=== squares[b] && squares[a] === squares[c]){
+        GameOver = true;
+        
+        return squares[a],GameOver;
+      }else{
+        return GameOver = false;
+      }
+    }
+    if(GameOver){
+      for(let i = 0; i<squares.length; i++){
+        squares[i] = null;
+      }
+    }
+    return null; //if no winner return null
   }
   function handleClick(i){
     if(squares[i]|| calcWinner[squares]){
       return;
     }
     const nextSquares = squares.slice();
-    const Player1 = squares[i]="X";
-    const Player2= squares[i]="O";
-    if(xIsNext){
-      nextSquares[i]= "X";
-    }else{
-      nextSquares[i]=  "O";
-    }
+    constSquares[i] = xIsNext? 'X':'O';
     setXNext(!xIsNext);
     setSquares(nextSquares);
   }
+  function restart(){
+    setSquares(Array(9).fill(null)); // Reset squares to an array with null values
+    setXNext(true);
+  }
+  const Winner = calcWinner(squares);
+  let status;
+  if(Winner){
+    status= `Game over Winner is:${squares[a]}`;
+  }else{
+    // if xIsNext set status to x else to O
+    status =  `Next player: ${xIsNext?'X':'O'}`;
+  }
   return (
   <>
-    <div className= "status">{status}
+    <div className= "status">{status}</div>
     <header>
-      <Restart />
+      <button className='restart' onClick={restart}>Restart</button>
     </header>
-    </div>
   <div className = "Container">
     <div className =  "Row">
-      <Square value = {squares[0]} onSquareClick={ ()=> handleClick(0)}/>
-      <Square value = {squares[1]} onSquareClick={ ()=> handleClick(1)}/>
-      <Square value ={squares[2]} onSquareClick={ ()=> handleClick(2)}/>
+      {[0,1,2].map((i) =>(
+        <Square key = {i} 
+        value = {squares[i]} 
+        onSquareClick = {() => handleClick(i)}/>
+      )
+      )}
+    {/*Replace all rows with code above*/}
     </div>
     <div className =  "Row">
       <Square value = {squares[3]} onSquareClick={ ()=> handleClick(3)}/>
@@ -66,31 +84,6 @@ export default function Board() {
     </div>
   </div>
   </> );
-    function calcWinner(squares){
-      let GameOver = null;
-      const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]];
-      for(let i = 0; i<lines.length; i++){
-        const[a,b,c]= lines[i];
-        if(squares[a] && squares[a]=== squares[b] && squares[a] === squares[c]){
-          GameOver = true;
-
-          return squares[a].GameOver;
-        }else{
-          return GameOver = false;
-        }
-      }
-      /*if(GameOver){
-
-      }*/
-      return null;
-    }
+    
 }
 
