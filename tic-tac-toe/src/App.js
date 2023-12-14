3ee5dac9783a42ff91da4a435b0d710aad6635ff
royboot <rgrizell467@gmail.com>
2023-12-14 20:49:09 +0100

    Winner
    Re-designed the winner code and made a timeout if somone hase won

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -22,16 +22,14 @@ export default function Board() {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         GameOver = true;
+        setTimeout(()=>{ 
+          setSquares(Array(9).fill(null));
+          setXNext(true);
+        },4000)
         return GameOver, squares[a];
       } else {
         return GameOver = false;
       }
-    }
-    if (GameOver) {
-      setTimeout(()=>{ 
-        setSquares(Array(9).fill(null));
-        setXNext(true);
-      },4000)
     }
     return null; //if no winner return null
   }
@@ -48,10 +46,12 @@ export default function Board() {
     setSquares(Array(9).fill(null)); // Reset squares to an array with null values
     setXNext(true);
   }
+  const Div = document.getElementById("Win");
   const Winner = calcWinner(squares);
   let status;
+  let winner;
   if (Winner) {
-    status = `Game over Winner is:${Winner}`;
+    winner = `The winner is ${Winner}`
   } else {
     // if xIsNext set status to x else to O
     status = `Next player: ${xIsNext ? 'X' : 'O'}`;
@@ -59,14 +59,17 @@ export default function Board() {
   return (
     <>
       <header>
-        <div className="status">{status}</div>
         <button className='restart' onClick={restart}>Restart</button>
       </header>
       <div className='Container'>
+      
+        <div className="status">{status}</div>
+        <div className='WinText' id='Win'>{winner}</div>
+     
         <div className='Row'>
         {[0, 1, 2].map((i) => (
             <Square 
-              key={i}
+              key={i}//indentifier for react
               value={squares[i]}
               onSquareClick={() => handleClick(i)}
             />))}
@@ -90,7 +93,7 @@ export default function Board() {
       />))}
       </div>
 
-      </div /*clossing tag for container div */>
+      </div>
     </>
   );
 
